46916ab1597b17219074d9d452b695dae40ed09a
jramosss <jramostod@gmail.com>
2025-02-05 21:37:38 -0300

    chord restrictor

--- lib/components/ChordsBox.tsx
+++ lib/components/ChordsBox.tsx
@@ -5,6 +5,9 @@ type ChordsBoxProps = {
   canEnterNewChords: boolean
 }
 
+const chordsRegex =
+  "/\b(?:G,C,D|A,B,C|E,C,D)|(?:[ABCDEFG](?:#|b)?)(?:/[ABCDEFG]b)?(?:(?:(?:maj|min|sus|add|aug|dim)(?:d{0,2}(?:#d{1,2}|susd)?)?)|(?:md{0,2}(?:(?:maj|add|#)d{0,2})?)|(?:-?d{0,2}(?:([^)]*)|#d{1,2})?))?"
+
 export default function ChordsBox({ canEnterNewChords }: ChordsBoxProps) {
   const [chords, setChords] = useState<string[]>([])
   const [currentChord, setCurrentChord] = useState<string>("")
@@ -21,6 +24,10 @@ export default function ChordsBox({ canEnterNewChords }: ChordsBoxProps) {
             value={currentChord}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
+                if (!currentChord.match(chordsRegex)) {
+                  console.error("Invalid chord")
+                  return
+                }
                 setChords([...chords, currentChord])
                 setCurrentChord("")
               }
